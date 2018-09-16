e7458774b9825110315fa616ddc741d4f11d6e6f
Omotola <omotola.babasola@yahoo.com>
2018-09-16 01:53:48 +0100

    Added db connection logic

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+var mysql = require('mysql');
+var config = require('./config');
+
+var con = mysql.createConnection({
+    host: config.db.MECHANIC_DB_URL,
+    user: config.db.MECHANIC_DB_USER,
+    password: config.db.MECHANIC_DB_PASSWORD
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 app.get('/', (req, res) => res.send('Welcome to Carmonic'));
 
 app.get('/getMechanics', (req, res) => res.send({'mechanicId':'0','latitude':'0','longitude':'0'}));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => con.connect(); console.log('Example app listening on port 3000!'));

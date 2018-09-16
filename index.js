510fb8fc9cb8abe9118d49bc59c57c10e1040937
Omotola <omotola.babasola@yahoo.com>
2018-09-16 11:28:58 +0100

    Added db connection logic successfully

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const app = express();
-var mysql = require('mysql');
-var config = require('./config');
+const { Pool } = require('pg');
+var config = require('./config.json');
 
-var con = mysql.createConnection({
-    host: config.db.MECHANIC_DB_URL,
-    user: config.db.MECHANIC_DB_USER,
-    password: config.db.MECHANIC_DB_PASSWORD
+const pool = new Pool({
+    user: config.MECHANIC_DB_USER,
+    host: config.MECHANIC_DB_ENDPOINT,
+    database: config.MECHANIC_DB_DATABASE,
+    password: config.MECHANIC_DB_PASSWORD,
+    port: 5432,
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
 });
 
 app.get('/', (req, res) => res.send('Welcome to Carmonic'));
 
 app.get('/getMechanics', (req, res) => res.send({'mechanicId':'0','latitude':'0','longitude':'0'}));
 
-app.listen(3000, () => con.connect(); console.log('Example app listening on port 3000!'));
+app.listen(3000, function() {
+    pool.query('SELECT * FROM "Mechanic" WHERE username = $1', [1], (err, res) => {
+        if (err) {
+            throw err
+        }
+        console.log('mechanic:', res.rows[0])
+    })
+    console.log('Example app listening on port 3000!');
+});
